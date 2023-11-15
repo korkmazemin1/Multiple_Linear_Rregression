4edcd3c96d1a53c3b6e1ccd864a5e5eed2287550
emin <korkmazzeminn1@gmail.com>
2023-11-15 22:51:55 +0300

    first_commit

---
+++ regression.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+facebook_data = pd.read_csv("dataset_Facebook.csv", sep=";")# dataset okundu
+
+X=facebook_data[["Category","Page total likes","Post Month","Post Hour","Post Weekday","Paid"]]# bağımsız değişkenler X ile belirtildi
+
+Y=facebook_data[["Total Interactions"]]# bağımlı değişken belirlendi
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)#veriler test ve eğitim verileri olarak ayrıldı
+
+
+
+# gradiant descent uygulanacak elindeki kaynağı dikkatli oku yeni kaynaklara bak en kısa yoldan uygulamaya geç
